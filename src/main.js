7e09a5b87f655b364dbb7e97abb547312810f5ba
denis_levchenko <tranzi90@gmail.com>
2022-07-01 23:24:03 +0300

    provide + inject pattern

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import FriendContact from './components/FriendContact.vue';
+import ActiveElement from '../../../8. Component Communication/cmp-communication-09-provide-inject-for-functions/src/components/ActiveElement.vue';
+import KnowledgeBase from '../../../8. Component Communication/cmp-communication-09-provide-inject-for-functions/src/components/KnowledgeBase.vue';
+import KnowledgeElement from '../../../8. Component Communication/cmp-communication-09-provide-inject-for-functions/src/components/KnowledgeElement.vue';
+import KnowledgeGrid from '../../../8. Component Communication/cmp-communication-09-provide-inject-for-functions/src/components/KnowledgeGrid.vue';
 
 const app = createApp(App);
 
-app.component('friend-contact', FriendContact);
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
 
 app.mount('#app');
