94c5dd6ad902117831a03ec12142bd278e51ac42
denis_levchenko <tranzi90@gmail.com>
2022-07-05 22:08:51 +0300

    learn forms and form elements

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,28 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps['another-prop'] }}</p>
-    </course-goals>
-  </div>
+  <the-form></the-form>
 </template>
 
 <script>
-import TheHeader from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/TheHeader.vue';
-import BadgeList from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/BadgeList.vue';
-import UserInfo from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/UserInfo.vue';
-import CourseGoals from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/CourseGoals.vue';
+import TheForm from './components/TheForm.vue';
 
 export default {
   components: {
-    TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
-  },
-  data() {
-    return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-};
+    TheForm
+  }  
+}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
 
 body {
   margin: 0;
+  background-color: #292929;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseBadge from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/BaseBadge.vue';
-import BaseCard from '../../../9. Diving Deeper Into Components/cmp-adv-06-scoped-slots/src/components/BaseCard.vue';
 
-const app = createApp(App);
-
-app.component('base-badge', BaseBadge);
-app.component('base-card', BaseCard);
-
-app.mount('#app');
+createApp(App).mount('#app');
