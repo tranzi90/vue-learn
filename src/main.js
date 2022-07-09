bbd784d8fcb03050e4ef90b541c6b3b75a0bc1b1
denis_levchenko <tranzi90@gmail.com>
2022-07-09 23:52:13 +0300

    vuex state management (using getters)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
-import BaseModal from './components/BaseModal.vue';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 2;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    }
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 3;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    }
+  }
+});
 
 const app = createApp(App);
 
-app.component('base-modal', BaseModal);
+app.use(store);
 
 app.mount('#app');
