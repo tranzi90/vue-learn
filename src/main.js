f04071029b887ddabb8e0279a9c316b51e8540b4
denis_levchenko <tranzi90@gmail.com>
2022-07-10 22:44:41 +0300

    vuex namespaced modules structure

--- src/main.js
+++ src/main.js
@@ -1,38 +1,7 @@
 import { createApp } from 'vue';
-import { createStore } from 'vuex';
 
 import App from './App.vue';
-
-const store = createStore({
-  state() {
-    return {
-      counter: 0
-    };
-  },
-  mutations: {
-    increment(state) {
-      state.counter = state.counter + 2;
-    },
-    increase(state, payload) {
-      state.counter = state.counter + payload.value;
-    }
-  },
-  getters: {
-    finalCounter(state) {
-      return state.counter * 3;
-    },
-    normalizedCounter(_, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) {
-        return 0;
-      }
-      if (finalCounter > 100) {
-        return 100;
-      }
-      return finalCounter;
-    }
-  }
-});
+import store from './store/index.js';
 
 const app = createApp(App);
 
