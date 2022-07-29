ccd60b080947a3a40c89a0db9041169d2d086967
denis_levchenko <tranzi90@gmail.com>
2022-07-29 22:57:56 +0300

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,68 @@
 <template>
-  <main>
-    <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
-    <projects-list :user="selectedUser"></projects-list>
-  </main>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
 </template>
 
-<script>
-import { ref } from 'vue';
-
-import USER_DATA from './dummy-data.js';
-
-import UserList from './components/users/UserList.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
-
-export default {
-  components: {
-    UserList,
-    ProjectsList,
-  },
-  setup() {
-    const selectedUser = ref(null);
-    const activeUsers = USER_DATA;
-
-    function selectUser(uid) {
-      selectedUser.value = activeUsers.find((usr) => usr.id === uid);
-    }
-
-    return { selectedUser, activeUsers, selectUser };
-  },
-};
-</script>
-
 <style>
-* {
-  box-sizing: border-box;
-}
-html {
-  font-family: sans-serif;
-}
-body {
-  margin: 0;
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
 }
 
-main {
-  display: flex;
-  justify-content: space-around;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-button {
-  font: inherit;
-  border: 1px solid #00006b;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
   background-color: transparent;
-  color: #00006b;
-  padding: 0.5rem 1.5rem;
-  cursor: pointer;
-  margin: 0.5rem 0.5rem 0.5rem 0;
-}
-button:hover,
-button:active {
-  background-color: #efefff;
 }
 
-button.selected {
-  background-color: #00006b;
-  color: white;
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 </style>
